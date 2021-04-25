d2f06dab68f207724e65b0ef0bc87f6a1fc571f2
AshutoshMoharana <ashutosh.moharana@yahoo.com>
2021-04-26 01:52:25 +0530

    this is a static website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const menuToggle = document.querySelector(".toggle");
-const showcase = document.querySelector(".toggle");
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.toggle');
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
